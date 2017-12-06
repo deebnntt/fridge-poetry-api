64d5a14d99f78c74a0df01bacdad188400064218
deebnntt <daniellebennettchan@gmail.com>
2017-12-06 13:39:10 -0500

    final changes before likes

--- app/models/poem.rb
+++ app/models/poem.rb
@@ -1,6 +1,6 @@
 class Poem < ApplicationRecord
 
-  belongs_to :user, optional: true
+  belongs_to :user
   belongs_to :poem_tags, optional: true
   has_many :magnets
   has_many :tags, through: :poems
